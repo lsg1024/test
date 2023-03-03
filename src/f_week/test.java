21c0aeef80e477de8fa9b4e55fd2347cdb3387f3
JS <zks145@naver.com>
2023-03-03 11:49:11 +0900

    test

---
+++ src/f_week/test.java
@@ -0,0 +1,14 @@
+package f_week;
+
+public class test {
+
+	public static void main(String[] args) {
+		int num = 10;
+		String str = "js";
+
+		System.out.println(num);
+		System.out.println(str);
+
+	}
+
+}
